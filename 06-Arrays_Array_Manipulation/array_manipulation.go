51c3e330b8d3b32399ba0208e18189293fc5491f
Marc Watters <marc.watters@proton.me>
2025-02-02 16:28:47 -0500

    06 - Arrays (Manipulation - Prefix Sum Algo)
    Starting with a 1-indexed array of zeros and a list of operations, for
    each operation add a value to each the array element between two given
    indices, inclusive. Once all operations have been performed, return the
    maximum value in the array.
    
    Example
    
    n = 10
    queries = [[1, 5, 3],[4, 8, 7],[6, 9, 1]
    
    Queries are interpreted as follows:
    
    a b k
    1 5 3
    4 8 7
    6 9 1
    
    Add the values of k between the indices a and b inclusive:
    
     1 2 3  4  5 6 7 8 9 10  <- index
    [0,0,0, 0, 0,0,0,0,0, 0] start
    [3,3,3, 3, 3,0,0,0,0, 0] query 1: 1, 5, 3
    [3,3,3,10,10,7,7,7,0, 0] query 2: 4, 8, 7
    [3,3,3,10,10,8,8,8,1, 0] query 3: 6, 8, 1
    
    The largest value is after all operations are performed.

---
+++ 06-Arrays_Array_Manipulation/array_manipulation.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+func arrayManipulation(n int32, queries [][]int32) int64 {
+	results := make([]int64, n+2)
+	var m int64
+	for _, q := range queries {
+		sIdx, eIdx, k := q[0], q[1], q[2]
+		results[sIdx] += int64(k)
+		results[eIdx+1] -= int64(k)
+	}
+
+	for i := 1; i < len(results); i++ {
+		results[i] += results[i-1]
+		if results[i] > m {
+			m = results[i]
+		}
+	}
+
+	return m
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	mTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
+
+	var queries [][]int32
+	for i := 0; i < int(m); i++ {
+		queriesRowTemp := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
+
+		var queriesRow []int32
+		for _, queriesRowItem := range queriesRowTemp {
+			queriesItemTemp, err := strconv.ParseInt(queriesRowItem, 10, 64)
+			checkError(err)
+			queriesItem := int32(queriesItemTemp)
+			queriesRow = append(queriesRow, queriesItem)
+		}
+
+		if len(queriesRow) != 3 {
+			panic("Bad input")
+		}
+
+		queries = append(queries, queriesRow)
+	}
+
+	result := arrayManipulation(n, queries)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
